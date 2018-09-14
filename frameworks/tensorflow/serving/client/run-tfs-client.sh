60f938d408787ad642878f01f85a305766891074
lucky <307479836@qq.com>
2018-09-14 11:28:01 +0800

    predict with saved model

---
+++ frameworks/tensorflow/serving/client/run-tfs-client.sh
@@ -0,0 +1,2 @@
+
+python ./mnist_client.py --num_tests=100 --server=localhost:9000
---
+++ frameworks/tensorflow/serving/server/run-tfserving.sh
@@ -0,0 +1,2 @@
+
+tensorflow_model_server --port=9000 --model_name=mnist --model_base_path=/root/my-model/mnist_model
