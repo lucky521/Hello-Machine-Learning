b1a2ef228b1ac5ceb9cffce161eb47fcf984515c
lucky <307479836@qq.com>
2018-09-26 08:21:17 +0800

    new sample

---
+++ frameworks/tensorflow/serving/inception_client.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2.7
+
+"""Send JPEG image to tensorflow_model_server loaded with inception model.
+"""
+
+from __future__ import print_function
+
+# This is a placeholder for a Google-internal import.
+
+import grpc
+import tensorflow as tf
+
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+
+
+tf.app.flags.DEFINE_string('server', 'localhost:9000',
+                           'PredictionService host:port')
+tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
+FLAGS = tf.app.flags.FLAGS
+
+
+def main(_):
+  channel = grpc.insecure_channel(FLAGS.server)
+  stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+  # Send request
+  with open(FLAGS.image, 'rb') as f:
+    # See prediction_service.proto for gRPC request/response details.
+    data = f.read()
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'inception'
+    request.model_spec.signature_name = 'predict_images'
+    request.inputs['images'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(data, shape=[1]))
+    result = stub.Predict(request, 10.0)  # 10 secs timeout
+    print(result)
+
+
+if __name__ == '__main__':
+  tf.app.run()
